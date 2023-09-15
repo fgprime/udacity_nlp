102d22b3890e5ec17b93e053a86493af2f316664
Franz Geiselbrechtinger <fg@primeinteractive.de>
2023-09-15 19:24:37 +0200

    feat: Add Udacity NLP implementation

---
+++ src/client/js/formHandler.js
@@ -0,0 +1,56 @@
+function showResults(data) {
+  const root = document.getElementById("results");
+
+  const getExistingContainer = root.firstChild;
+  if (getExistingContainer) root.removeChild(getExistingContainer);
+
+  const container = document.createElement("div");
+
+  let agreement = document.createElement("div");
+  agreement.textContent = `Agreement: ${data.agreement}`;
+  container.appendChild(agreement);
+
+  let irony = document.createElement("div");
+  irony.textContent = `Irony: ${data.irony}`;
+  container.appendChild(irony);
+
+  let subjectivity = document.createElement("div");
+  subjectivity.textContent = `Subjectivity: ${data.subjectivity}`;
+  container.appendChild(subjectivity);
+
+  root.appendChild(container);
+}
+
+async function analyzeText(formData) {
+  return await fetch("http://localhost:8081/api", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then(function (data) {
+      return data;
+    });
+}
+
+async function handleSubmit(event) {
+  event.preventDefault();
+
+  const formElement = document.querySelector("form");
+  const formData = new FormData(formElement);
+
+  if (!formData.get("text") || formData.get("text").length === 0) {
+    alert("Please input a text in the input field.");
+    return;
+  }
+  const formDataObject = Object.fromEntries(formData.entries());
+
+  const textInformation = await analyzeText(formDataObject);
+  showResults(textInformation);
+}
+
+export { handleSubmit };
---
+++ src/server/API.js
@@ -0,0 +1,30 @@
+/* Global Variables */
+const dotenv = require("dotenv");
+dotenv.config();
+
+const meaningcloudService = "https://api.meaningcloud.com/sentiment-2.1";
+const meaningcloudApiKey = process.env.API_KEY;
+
+console.log(`Your API key is ${meaningcloudApiKey}`);
+
+async function getMeaning(text) {
+  let queries = [];
+  queries.push(`lang=en`);
+  queries.push(`key=${meaningcloudApiKey}`);
+  queries.push(`txt=${encodeURIComponent(text)}`);
+
+  const query = queries.join("&");
+  console.dir(query);
+  const url = `${meaningcloudService}?${query}`;
+  const response = await fetch(url, {});
+  try {
+    const data = await response.json();
+    console.dir(data);
+    return data;
+  } catch (error) {
+    console.log("Error", error);
+  }
+  console.log("Hallo");
+  return "HALLO";
+}
+module.exports = { getMeaning };
